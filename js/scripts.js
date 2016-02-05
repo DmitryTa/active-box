08711de52bd7c74565aa4accf68fa7e19a825ad6
DmitryTa <tskuadm@gmail.com>
2016-02-05 18:14:00 +0300

    first gh-pages commit

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function(){
+  $('.slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    swipe: false,
+    pauseOnHover: false,
+	speed: 800,
+	fade: true,
+	cssEase: 'linear'
+  });
+});
+   
